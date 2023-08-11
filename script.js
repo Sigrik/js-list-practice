644d4b6dd782f485e4cc93e1de60aa61dfa9f1e3
Sigrik <sigrikpoczta@gmail.com>
2023-08-11 19:30:33 +0200

    Add code for displaying products

--- script.js
+++ script.js
@@ -5,13 +5,28 @@ const itemSize = ["L", "M", "S"];
 
 for (let i = 0; i < 100; ++i) {
   products.push({
-      color: itemColor[Math.floor(Math.random() * itemColor.length)],
-      type: itemType[Math.floor(Math.random() * itemType.length)],
-      size: itemSize[Math.floor(Math.random() * itemSize.length)],
-      price: Math.floor(Math.random() * (200 - 25) + 25)
-    });
+    color: itemColor[Math.floor(Math.random() * itemColor.length)],
+    type: itemType[Math.floor(Math.random() * itemType.length)],
+    size: itemSize[Math.floor(Math.random() * itemSize.length)],
+    price: Math.floor(Math.random() * (200 - 25) + 25),
+  });
 }
 
-itemList.forEach((product) => {
+products.forEach((product) => {
   console.log(product);
+});
+
+const productsContainer = document.querySelector(".products-container");
+
+products.forEach((product) => {
+  console.log(product);
+  const productContent = document.createElement("div");
+  const productText = document.createTextNode(
+    product.color + " " +
+    product.type + " " +
+    product.size + " " +
+    product.price
+  );
+  productContent.appendChild(productText);
+  productsContainer.appendChild(productContent);
 });
