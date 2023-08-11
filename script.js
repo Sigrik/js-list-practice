6297b3dd388e47e115310b56babe2b85e73a4653
Sigrik <sigrikpoczta@gmail.com>
2023-08-11 19:14:33 +0200

    Change all instances of 'item' to 'product' for cohesiveness

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 let products = [];
-const itemColor = ["black", "white", "brown", "red"];
-const itemType = ["t-shirt", "trousers", "jacket"];
-const itemSize = ["L", "M", "S"];
+const productColor = ["black", "white", "brown", "red"];
+const productType = ["t-shirt", "trousers", "jacket"];
+const productSize = ["L", "M", "S"];
 
 for (let i = 0; i < 100; ++i) {
   products.push({
-      color: itemColor[Math.floor(Math.random() * itemColor.length)],
-      type: itemType[Math.floor(Math.random() * itemType.length)],
-      size: itemSize[Math.floor(Math.random() * itemSize.length)],
+      color: productColor[Math.floor(Math.random() * productColor.length)],
+      type: productType[Math.floor(Math.random() * productType.length)],
+      size: productSize[Math.floor(Math.random() * productSize.length)],
       price: Math.floor(Math.random() * (200 - 25) + 25)
     });
 }
 
-itemList.forEach((product) => {
-  console.log(product);
-});
