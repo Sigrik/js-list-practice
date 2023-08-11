3ead007024dfa090995ae520122b2f933828c16b
Sigrik <sigrikpoczta@gmail.com>
2023-08-11 16:30:18 +0200

    Fix the populating loop for the array and rename the array

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let itemList = [];
+let products = [];
 const itemColor = ["black", "white", "brown", "red"];
 const itemType = ["t-shirt", "trousers", "jacket"];
 const itemSize = ["L", "M", "S"];
 
-for (i = 0; i < 100; ++i) {
-  itemList["item" + i] =
-    {
+for (let i = 0; i < 100; ++i) {
+  products.push({
       color: itemColor[Math.floor(Math.random() * itemColor.length)],
       type: itemType[Math.floor(Math.random() * itemType.length)],
       size: itemSize[Math.floor(Math.random() * itemSize.length)],
       price: Math.floor(Math.random() * (200 - 25) + 25)
-    };
+    });
 }
+
+itemList.forEach((product) => {
+  console.log(product);
+});
