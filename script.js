06c7e502981f5c0ea01a8bb9f4e6777f9a5f01d5
Sigrik <sigrikpoczta@gmail.com>
2023-08-14 21:22:05 +0200

    Add function for filtering the products, add currency to the product display

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const products = generateProducts();
+const products = filterProducts("color", "white");
 renderTable(products);
 
 function generateProducts() {
   const products = [];
   const productColor = ["Black", "White", "Brown", "Red"];
-  const productType = ["T-shirt", "Trousers", "Jacket"];
+  const productType = ["Koszulka", "Spodnie", "Kurtka"];
   const productSize = ["L", "M", "S"];
   for (let i = 0; i < 100; ++i) {
     products.push({
@@ -14,7 +14,7 @@ function generateProducts() {
       price: Math.floor(Math.random() * (200 - 25) + 25),
     });
   }
-  return products;
+  return products
 }
 
 function renderTable(products) {
@@ -23,9 +23,49 @@ function renderTable(products) {
     console.log(product);
     const productContent = document.createElement("div");
     const productText = document.createTextNode(
-      `${product.color} ${product.type} ${product.size} ${product.price}`
+      `${product.color} ${product.type} ${product.size} ${product.price} zł`
     );
     productContent.appendChild(productText);
     productsContainer.appendChild(productContent);
+  });
+}
+
+function filterProducts(productAttribute, attributeValue) {
+  const products = generateProducts();
+  const attribute = productAttribute;
+  const value = attributeValue;
+  return products.filter((product) => {
+    switch (attribute, value) {
+      case "color", "black":
+        return product.color === "Black";
+        break;
+      case "color", "white":
+        return product.color === "White";
+        break;
+      case "color", "other":
+        return product.color !== "Black" || "White"; // not working as intended, needs fixing
+        break;
+      case "color", "all":
+        return product.color;
+        break;
+      case "type", "t-shirt":
+        return product.type === "Koszulka";
+        break;
+      case "type", "trousers":
+        return product.type === "Spodnie";
+        break;
+      case "type", "jacket":
+        return product.type === "Kurtka";
+        break;
+      case "size", "L":
+        return product.type === "L";
+        break;
+      case "size", "M":
+        return product.type === "M";
+        break;
+      case "size", "S":
+        return product.type === "S";
+        break;
+    }
   });
 }
