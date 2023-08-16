0e694ee462b0d04c50d3bc333b2047114eacec68
Sigrik <sigrikpoczta@gmail.com>
2023-08-16 15:46:34 +0200

    Refactor filters data and populating loop

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
-const products = filterProducts("color", "other");
-renderTable(products);
+const productSort = {
+  label: "Sortowanie",
+  name: "sort",
+  id: "sort",
+  options: ["asc", "desc", "default"],
+};
 
-const filters = {
-  color: null,
-  type: null,
-  size: null,
-  price: null
-}
+const productFilterColor = {
+  label: "Kolor",
+  name: "color-filter",
+  id: "color-filter",
+  options: ["Black", "White", "Brown", "Red"], // + All
+};
+
+const productFilterType = {
+  label: "Typ ubrania",
+  name: "type-filter",
+  id: "type-filter",
+  options: ["Koszulka", "Spodnie", "Kurtka"], // + All
+};
+
+const productFilterSize = {
+  label: "Rozmiar",
+  name: "size-filter",
+  id: "size-filter",
+  options: ["L", "M", "S"], // + All
+};
+
+const products = generateProducts();
+//const products = filterProducts("color", "other");
+renderTable(products);
 
 function generateProducts() {
   const products = [];
-  const productColor = ["Black", "White", "Brown", "Red"];
-  const productType = ["Koszulka", "Spodnie", "Kurtka"];
-  const productSize = ["L", "M", "S"];
   for (let i = 0; i < 100; ++i) {
     products.push({
-      color: productColor[Math.floor(Math.random() * productColor.length)],
-      type: productType[Math.floor(Math.random() * productType.length)],
-      size: productSize[Math.floor(Math.random() * productSize.length)],
+      color: productFilterColor.options[Math.floor(Math.random() * productFilterColor.options.length)],
+      type: productFilterType.options[Math.floor(Math.random() * productFilterType.options.length)],
+      size: productFilterSize.options[Math.floor(Math.random() * productFilterSize.options.length)],
       price: Math.floor(Math.random() * (200 - 25) + 25),
     });
   }
-  return products
+  return products;
 }
 
 function renderTable(products) {
   const productsContainer = document.querySelector(".products-container");
   products.forEach((product) => {
-    console.log(product);
     const productContent = document.createElement("div");
     const productText = document.createTextNode(
       `${product.color} ${product.type} ${product.size} ${product.price} zł`
@@ -37,42 +55,68 @@ function renderTable(products) {
   });
 }
 
+const filters = {
+  color: null,
+  type: null,
+  size: null,
+  price: null,
+};
+
 function filterProducts(productAttribute, attributeValue) {
   const products = generateProducts();
   const attribute = productAttribute;
   const value = attributeValue;
   return products.filter((product) => {
-    switch (attribute, value) {
-      case "color", "black":
+    switch ((attribute, value)) {
+      case ("color", "black"):
         return product.color === "Black";
         break;
-      case "color", "white":
+      case ("color", "white"):
         return product.color === "White";
         break;
-      case "color", "other":
+      case ("color", "other"):
         return product.color !== "Black" && product.color !== "White";
         break;
-      case "color", "all":
+      case ("color", "all"):
         return product.color;
         break;
-      case "type", "t-shirt":
+      case ("type", "t-shirt"):
         return product.type === "Koszulka";
         break;
-      case "type", "trousers":
+      case ("type", "trousers"):
         return product.type === "Spodnie";
         break;
-      case "type", "jacket":
+      case ("type", "jacket"):
         return product.type === "Kurtka";
         break;
-      case "size", "L":
+      case ("size", "L"):
         return product.type === "L";
         break;
-      case "size", "M":
+      case ("size", "M"):
         return product.type === "M";
         break;
-      case "size", "S":
+      case ("size", "S"):
         return product.type === "S";
         break;
     }
   });
 }
+
+/*
+function renderFilterOptions() {
+  const filterContainer = document.querySelector(".filter-container");
+  products.forEach((product) => {
+    const filterContent = document.createElement("select");
+    const filterText = document.createTextNode(
+      `${product.color} ${product.type} ${product.size} ${product.price} zł`
+    );
+    filterContent.appendChild(filterText);
+    filterContainer.appendChild(filterContent);
+  });
+}
+
+
+renderFilterOptions(productFilter1);
+renderFilterOptions(productFilter2);
+renderFilterOptions(productFilter3);
+*/
